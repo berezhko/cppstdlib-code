07114e5b666d076ecba34daf79dad64dd79b9b08
Ivan A. Berezhko <it@ues.ru>
2015-12-20 16:25:13 +0800

    Add library -lm -lrt

--- gen-cmake.sh
+++ gen-cmake.sh
@@ -12,6 +12,7 @@ do
         (
             echo "cmake_minimum_required(VERSION 2.8)"
             echo "add_definitions(-std=c++11)"
+            echo "set(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -lrt -lm\")"
             for i in *cpp
             do
                 echo "add_executable($j-`basename $i .cpp` $i)"
